61ae7e06023658e246f03e74f2c5e3e7bf72893f
alan <alanseiki55@gmail.com>
2023-11-18 15:55:42 -0300

    exercicio5melhorado

--- src/app/ex5/ex5.component.ts
+++ src/app/ex5/ex5.component.ts
@@ -6,14 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ex5.component.scss'],
 })
 export class Ex5Component  implements OnInit {
-  numero1: number = 0;
-  numero2: number = 0;
+  numero1?: number;
+  numero2?: number;
   operacao: string = 'soma';
   resultado: string = '';
   valor: number = 0;
   constructor() { }
   
   calcular(){
+    if (this.numero1 === undefined) {
+      this.numero1 = 1
+    } 
+    if (this.numero2 === undefined) {
+      this.numero2 = 1
+    } 
     if(this.operacao == 'soma'){
       this.valor = this.numero1+this.numero2;
       this.resultado = `${this.valor}`;
